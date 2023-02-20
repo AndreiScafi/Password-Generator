70082690c758ea0647b6fb23e06d75287f8df555
Andrei Scafi <andreiscafi@hotmail.com>
2023-02-20 09:04:52 -0300

    Generate Password with JavaScript - Gerando o Password com JavaScript.

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const clipboardEl = document.getElementById('clipboard');
+const generateBtn = document.getElementById('generate');
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
+}
+
+generateBtn.addEventListener('click', () => {
+    const length = +lengthEl.value; // the + sign converts the string to a number
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumbers = numbersEl.checked;
+    const hasSymbols = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumbers, hasSymbols, length);
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    if (typesCount === 0) {
+        return ''
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
 }
 
 
@@ -23,10 +63,3 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
-/* for (let i = 0; i < 10; i++) {
-    console.log(getRandomLower())
-    console.log(getRandomUpper())
-    console.log(getRandomNumber())
-    console.log(getRandomSymbol())
-}
- */
